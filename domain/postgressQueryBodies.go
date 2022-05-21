b8891f8e3eaa976a1fc586fe4f0edaa07657cb3c
Iulian Olaru <iulianolaru249@yahoo.com>
2022-05-21 13:35:36 +0300

    Ato Generate UIDs

--- domain/postgressQueryBodies.go
+++ domain/postgressQueryBodies.go
@@ -1,13 +1,10 @@
 package domain
 
 type AddMessHallInfoQuery struct {
-	MessHallAdminUID      string
 	MessHallAdminNickname string
-	MessHallUID           string
 	Street                string
 	City                  string
 	Country               string
-	MenuUID               string
 	Status                string
 	AttendanceNumber      int
 }
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.16
 
 require (
 	github.com/emicklei/go-restful v2.15.0+incompatible
+	github.com/google/uuid v1.3.0 // indirect
 	repositories v0.0.0-00010101000000-000000000000
 	submarineapi v0.0.0-00010101000000-000000000000
 	usecases v0.0.0-00010101000000-000000000000 // indirect
